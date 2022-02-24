eb519c75a15c9c6bab4a8cdf01397cabf668ed6c
sorachung <sorachung.src@gmail.com>
2022-02-24 12:01:18 -0600

    added moose responses; empty question exits

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 Main();
 
 void Main()
 {
     MooseSays("Ask me a question, any question!");
+    bool isQuestionNotValid = String.IsNullOrWhiteSpace(Console.ReadLine());
+    if (!isQuestionNotValid)
+    {
+        MooseResponds();
+    }
 }
 
 void MooseSays(string message)
@@ -37,4 +44,36 @@ void MooseSays(string message)
                       \ \'._  ` '_.'
                        `^^` `^^^`
     ");
+}
+
+void MooseResponds()
+{
+    List<string> responses = new List<string>()
+    {
+        "As I see it, yes.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don’t count on it.",
+        "It is certain.",
+        "It is decidedly so.",
+        "Most likely.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Outlook good.",
+        "Reply hazy, try again.",
+        "Signs point to yes.",
+        "Very doubtful.",
+        "Without a doubt.",
+        "Yes.",
+        "Yes – definitely.",
+        "You may rely on it."
+    };
+
+    Random r = new Random();
+    int randomNumber = r.Next(0, 19);
+
+    MooseSays(responses[randomNumber]);
 }
